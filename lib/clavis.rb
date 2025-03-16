bea655a81a82f7b1bdc79bf907f814868257378f
Clayton Lengel-Zigich <6334+clayton@users.noreply.github.com>
2025-03-16 11:28:10 -0700

    v0.1.1

---
+++ lib/clavis.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clavis/version"
+
+module Clavis
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clavis/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clavis
+  VERSION = "0.1.1"
+end
