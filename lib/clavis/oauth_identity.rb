3202777c70e449f87b045c329019556afbec1a1f
Clayton Lengel-Zigich <6334+clayton@users.noreply.github.com>
2025-03-17 14:52:37 -0700

    Fix Rails version check in OauthIdentity
    - Use Rails::VERSION::STRING instead of Rails.gem_version which doesn't exist

--- lib/clavis/oauth_identity.rb
+++ lib/clavis/oauth_identity.rb
@@ -10,7 +10,8 @@ module Clavis
     validates :uid, uniqueness: { scope: :provider }
 
     # Use serialize with a single argument if Rails 7.1+ is detected
-    if defined?(Rails) && Rails.gem_version >= Gem::Version.new("7.1")
+    if defined?(Rails::VERSION) &&
+       Gem::Version.new(Rails::VERSION::STRING) >= Gem::Version.new("7.1")
       serialize :auth_data
     else
       serialize :auth_data, JSON
