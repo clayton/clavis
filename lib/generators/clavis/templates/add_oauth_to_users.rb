cef3c75a17f8c58883b5cf019bbfc17f07e77449
Clayton Lengel-Zigich <6334+clayton@users.noreply.github.com>
2025-03-18 16:31:21 -0700

    Clean up duplicate install generators and fix missing template

---
+++ lib/generators/clavis/templates/add_oauth_to_users.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddOauthToUsers < ActiveRecord::Migration[6.0]
+  def change
+    # Skip if any of these columns already exist
+    return if column_exists?(:users, :provider) || column_exists?(:users, :uid)
+
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :oauth_token, :string
+    add_column :users, :oauth_expires_at, :datetime
+    add_column :users, :oauth_refresh_token, :string
+    add_column :users, :oauth_avatar_url, :string
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end 
